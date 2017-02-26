3db611dfa4100eba51b0f5b53505b2297d1c5685
BamRu <is641ryjkov@mail.ru>
2017-02-26 23:16:06 +0700

    entry

---
+++ credit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{	
+	int s,d;
+	
+	printf ("Vvedite summy vklada:");
+	scanf ("%d",&s);
+	printf ("Vvedite srok vklada:");
+	scanf ("%d",&d); 
+	
+
+	if (((s>9999)&&(s<100001))&&((d>0)&&(d<366)))
+    {
+        	    	
+	}
+	else
+	  printf ("Nekorektno vvedeni znacheniya!!");
+	  	
+	
+	
+	
+	return 0;
+}
